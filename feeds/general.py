fa6cf7ac3211927891176b029e7bca2f908e34ae
Puzzlet Chung <puzzlet@gmail.com>
2009-03-24 15:27:43 +0000

    general.py, title_only.py, title_time.py 하나로 합침
    git-svn-id: https://dev.upnl.org/svn/ircbot2/trunk@8 66f668a4-574d-4515-85ec-6e73d9c367eb

--- feeds/general.py
+++ feeds/general.py
@@ -2,31 +2,45 @@
 import os.path
 import re
 from util import force_unicode
-from config import FEEDEX_ROOT
+
+FILE_PATH = os.path.dirname(__file__)
 
 def load():
-    result = []
-    for line in open(os.path.join(FEEDEX_ROOT, 'feeds/general.data'), 'r'):
+    format = {}
+    for line in open(os.path.join(FILE_PATH, 'format'), 'r'):
         line = line.strip().decode('utf-8')
-        if line.startswith('#'): continue
-        if not line: continue
+        if line.startswith('#'):
+            continue
+        if ' ' not in line:
+            continue
+        tokens = re.split(r'\s+', line, 1)
+        format[tokens[0]] = tokens[1]
+    result = []
+    for line in open(os.path.join(FILE_PATH, 'general.data'), 'r'):
+        line = line.strip().decode('utf-8')
+        if line.startswith('#'):
+            continue
+        if not line:
+            continue
         tokens = re.split(r'\s+', line)
-        result.append(parse(tokens))
+        result.append(parse(tokens, format))
     return result
 
-def parse(argv):
+def parse(argv, format):
     data = {
         'name': argv[0],
         'target': argv[1],
-        'uri': argv[2],
+        'format': format[argv[2]],
+        'uri': argv[3],
         }
     return data['uri'], data
 
 def display(entry, data):
     kwargs = dict(data)
+    kwargs['bold'] = '\x02'
     kwargs['link'] = force_unicode(entry.link)
     kwargs['title'] = force_unicode(entry.title)
-    msg = u'[%(name)s] \u0002%(link)s\u0002 [%(title)s] (%(time)s)' % kwargs
+    msg = kwargs['format'] % kwargs
     return data['target'], msg, {}
 
 channels = set()
