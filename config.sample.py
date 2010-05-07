756da91a4ce95062897acb3009ae3f1841a703cc
Puzzlet Chung <puzzlet@gmail.com>
2010-05-07 04:56:55 +0000

    [feedex] modify config
    git-svn-id: https://dev.upnl.org/svn/ircbot2/trunk@85 66f668a4-574d-4515-85ec-6e73d9c367eb

--- config.sample.py
+++ config.sample.py
@@ -1,18 +1,7 @@
-import os.path
-FEEDEX_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-NICKNAME_LIST = ['FedEx']
-DEBUG_MODE = True
-
-SERVER_LIST = [
-    ('irc.hanirc.org', 6665),
-]
-USE_SSL = False
-MAX_CHAR = 300
-BUFFER_PERIOD = 1
-
-FETCH_PERIOD = 3
-FREQUENT_FETCH_PERIOD = 20
-FUTURE_THRESHOLD = 86400
-TIMEZONE = 9
-
+{
+    'version': 2010050701, # increment this to reload
+    'debug': False,
+    'server': ('irc.hanirc.org', 6665),
+    'nickname': b'FeedEx',
+    'use_ssl': False,
+}
